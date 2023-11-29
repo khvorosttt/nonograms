5356f07182ad97b136421a0511340012e768d33d
khvorosttt <khvorosttt@yandex.by>
2023-11-29 20:22:39 +0300

    feat: add a popup menu

--- coffee-house/burger.js
+++ coffee-house/burger.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector("#burger");
+const menu__nav = document.querySelector("#menu__nav");
 burger.addEventListener("click", burgerClick);
+
 
 function burgerClick () {
     burger.classList.toggle("menu__burger-active");
+    menu__nav.classList.toggle("active");
 }
