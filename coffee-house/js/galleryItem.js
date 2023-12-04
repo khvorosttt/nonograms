a3a46e37c33af8173eb8df145cb8d280c5618d06
khvorosttt <khvorosttt@yandex.by>
2023-12-05 02:28:12 +0300

    feat: add cards when selecting a category

---
+++ coffee-house/js/galleryItem.js
@@ -0,0 +1,13 @@
+const galleryItem = (item) => `
+    <div class="gallery-item">
+        <div class="gallery-item-img">
+            <img src="./img/png/coffee-1.png" alt="${item.name}">
+        </div>
+        <div class="gallery-item-context">
+            <h3 class="item__name">${item.name}</h3>
+            <p class="item-description">${item.description}</p>
+            <h3 class="item__cost">$${item.price}</h3>
+        </div>
+    </div>
+`
+export default galleryItem;
--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -1,17 +1,49 @@
 import info from '../products.json' assert { type: "json" };
+import galleryItem from './galleryItem.js';
 
 const gallery_column = document.querySelector(".gallery_column");
 const tab__items = document.querySelectorAll(".tab__item");
+const addCards = document.querySelector(".add__cards");
+const addCardsStyle = getComputedStyle(addCards);
+let currentCategory = "coffee";
+let categoryData = [];
+let categoryDataShow = [];
+const stepShow = 4;
 
 function menuList(event) {
+    categoryDataShow = [];
     const currentElem = event.currentTarget;
-    console.log(currentElem);
     tab__items.forEach( (tab) => {
         tab.classList.remove("tab__item-active");
     });
     currentElem.classList.add("tab__item-active");
-    console.log(currentElem.textContent);
+    currentCategory = currentElem.textContent.toLowerCase().trim();
     gallery_column.replaceChildren();
+    categoryData = info.filter(item => item.category === currentCategory);
+    let displayAddButton = addCardsStyle.display;
+    if(displayAddButton == 'none') {
+        console.log('klmjnhbgvfcd');
+        categoryData.forEach( (item) => {
+            categoryDataShow.push(item);
+            gallery_column.insertAdjacentHTML('beforeend',galleryItem(item).toString());
+        })
+    } else {
+        for(let i=0; i<stepShow; i++) {
+            categoryDataShow.push(categoryData[i]);
+            gallery_column.insertAdjacentHTML('beforeend',galleryItem(categoryData[i]).toString());
+        }
+    }
+    if(categoryDataShow.length === categoryData.length) {
+        addCards.style.display = 'none';
+    } else {
+        addCards.style.display = '';
+        console.log('////////////////////');
+    }
+
+}
+
+function galleryItems() {
+
 }
 
 tab__items.forEach( (tab) => {
