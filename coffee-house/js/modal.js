f0fdaf57f71a3f9eb3b6494205f2cf5737694cf0
khvorosttt <khvorosttt@yandex.by>
2023-12-07 15:12:37 +0300

    feat: Fix error in binding multiple click events to one element

--- coffee-house/js/modal.js
+++ coffee-house/js/modal.js
@@ -8,10 +8,12 @@ const modalPrice = document.querySelector(".modal__total-price");
 const modalImg = document.querySelector('.modal__img');
 let priceSize;
 let priceAdditives = 0;
+let item;
 
-export function showModal(item) {
+export function showModal(currentItem) {
+    item = currentItem;
     modal__background.classList.add('modal-active');
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.add("no-scroll");
     modalImg.src = item.img;
     modalImg.alt = item.name;
     modal__name.innerHTML = item.name;
@@ -31,62 +33,75 @@ export function showModal(item) {
         modal__additivesButtons[j].querySelector('.additives-item-name').innerHTML = item.additives[j].name;
     }
     modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
+}
 
-
-
-
+function changeSize(event) {
+    const currentElem = event.currentTarget;
     modal__sizeButtons.forEach( (button) => {
-        button.addEventListener("click", (event) => {changeSize(event);});
+        button.classList.remove("size-item-active");
     });
-    
-    function changeSize(event) {
-        const currentElem = event.currentTarget;
-        modal__sizeButtons.forEach( (button) => {
-            button.classList.remove("size-item-active");
-        });
-        currentElem.classList.add("size-item-active");
-        let newSize = currentElem.querySelector('.size-item-size').textContent.trim().toLowerCase();
-        priceSize = parseFloat(item.price) + parseFloat(item.sizes[newSize]["add-price"]);
-        modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
-    }
+    currentElem.classList.add("size-item-active");
+    let newSize = currentElem.querySelector('.size-item-size').textContent.trim().toLowerCase();
+    priceSize = parseFloat(item.price) + parseFloat(item.sizes[newSize]["add-price"]);
+    modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
+}
 
-   
-    modal__additivesButtons.forEach( (button) => {
-        button.addEventListener("click", (event) => {changeAdditives(event);})
-    })
-
-    function changeAdditives(event) {
-        const currentElem = event.currentTarget;
-        currentElem.classList.toggle("additives-item-active");
-        console.log(currentElem.querySelector('.additives-item-name').textContent);
-        const nameAddAdditive = currentElem.querySelector('.additives-item-name').textContent.trim();
-        const addAdditive = item.additives.find(additive => additive.name === nameAddAdditive);
-        console.log(addAdditive);
-        const priceAddAdditive = addAdditive["add-price"];
-        if(currentElem.classList.contains("additives-item-active")){
-            priceAdditives += parseFloat(priceAddAdditive);
-        } else {
-            priceAdditives -= parseFloat(priceAddAdditive);
-        }
-        modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
+function changeAdditives(event) {
+    const currentElem = event.currentTarget;
+    currentElem.classList.toggle('additives-item-active');
+    const nameAddAdditive = currentElem.querySelector('.additives-item-name').textContent.trim();
+    const addAdditive = item.additives.find(additive => additive.name === nameAddAdditive);
+    const priceAddAdditive = addAdditive["add-price"];
+    if(currentElem.classList.contains('additives-item-active')){
+        priceAdditives += parseFloat(priceAddAdditive);
+    } else {
+        priceAdditives -= parseFloat(priceAddAdditive);
     }
+    modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
+}
+
+function changeAdditives(event) {
+    const currentElem = event.currentTarget;
+    currentElem.classList.toggle('additives-item-active');
+    const nameAddAdditive = currentElem.querySelector('.additives-item-name').textContent.trim();
+    const addAdditive = item.additives.find(additive => additive.name === nameAddAdditive);
+    const priceAddAdditive = addAdditive["add-price"];
+    if(currentElem.classList.contains('additives-item-active')){
+        priceAdditives += parseFloat(priceAddAdditive);
+    } else {
+        priceAdditives -= parseFloat(priceAddAdditive);
+    }
+    modalPrice.innerHTML = `$${(priceSize + priceAdditives).toFixed(2)}`;
 }
 
 function closeModal() {
     modal__background.classList.remove('modal-active');
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.remove("no-scroll");
     modal__sizeButtons.forEach( (button) => {
         button.classList.remove("size-item-active");
     });
     modal__additivesButtons.forEach( (button) => {
         button.classList.remove("additives-item-active");
     });
+    priceAdditives = 0
 }
 
+
+
+
+modal__sizeButtons.forEach( (button) => {
+    button.addEventListener("click", (event) => {changeSize(event);});
+});
+
+modal__additivesButtons.forEach( (button) => {
+    button.addEventListener("click", (event) => {changeAdditives(event);})
+});
+
 modalClose.addEventListener("click", closeModal);
+
 modal__background.addEventListener("click", (event) => {
     if(event.target === modal__background) {
-        closeModal()
+        closeModal();
     }
 });
 
