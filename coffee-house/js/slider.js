b7d846a1a7fada7ef49b60e72be85f674fb4c2aa
khvorosttt <khvorosttt@yandex.by>
2023-12-11 15:33:34 +0300

    refactor: remove all methods with setInterval, fill the progress bar with animation

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -2,7 +2,6 @@ const previous = document.querySelector("#previous");
 const next = document.querySelector("#next");
 const rowSlider = document.getElementById("row__slider");
 const slides = document.querySelectorAll('.slide');
-const progress = document.querySelectorAll(".progress");
 let currentSlide = 0;
 let shift = 0;
 const rowSliderStyle = getComputedStyle(rowSlider);
@@ -10,35 +9,8 @@ let gapWidth = parseInt(rowSliderStyle.getPropertyValue('gap'),10)
 const lastSlide = slides.length - 1;
 let slideWidth = slides[0].clientWidth;
 let slideSize = slideWidth + gapWidth;
-const slideTime = 5000;
-let slideInterval;
-const stepProgress = 10;
-let progressSize = 0;
-const startProgress = 0;
-const finishProgress = 100;
-const progressTime = slideTime / stepProgress;
-let progressInterval;
-let startTouch;
-let endTouch;
-const minDist = slideWidth / 4;
-
-
-function progressSlide(slide) {
-    if (progressSize < finishProgress) {
-        progressSize += stepProgress;
-        progress[slide].style.width = `${progressSize}%`;
-    } else {
-        progressSize = startProgress;
-        progress[slide].style.width = `${startProgress}%`;
-        stopInterval();
-        startInterval();
-        nextSlide();
-    }
-}
 
 function previousSlide() {
-    progress[currentSlide].style.width = `${startProgress}%`;
-    progressSize = startProgress;
     currentSlide = (currentSlide === 0) ? lastSlide : (currentSlide - 1);
     if (currentSlide === lastSlide) {
         shift -= currentSlide * slideSize;
@@ -49,8 +21,6 @@ function previousSlide() {
 }
 
 function nextSlide() {
-    progress[currentSlide].style.width = `${startProgress}%`;
-    progressSize = startProgress;
     currentSlide = (currentSlide === lastSlide) ? 0 : (currentSlide + 1);
     if (currentSlide === 0) {
         shift += lastSlide * slideSize;
@@ -60,50 +30,9 @@ function nextSlide() {
     rowSlider.style.transform = `translate(${shift}px)`;
 }
 
-function startInterval() {
-    progressInterval = setInterval( () => {
-        progressSlide(currentSlide);
-    }, progressTime);
-}
-
-function stopInterval() {
-    clearInterval(progressInterval);
-}
-
-function touchStart(event) {
-    stopInterval();
-    startTouch = event.touches[0].clientX;
-}
-
-function touchEnd(event) {
-    endTouch = event.changedTouches[0].clientX;
-    let distance = startTouch - endTouch;
-    if(Math.abs(distance)<minDist) {
-        startInterval();
-    } else {
-        if(distance>0){
-            startInterval();
-            nextSlide();
-        } else {
-            startInterval();
-            previousSlide();
-        }
-    }
-}
-
 previous.addEventListener("click", () => {
-    stopInterval();
-    startInterval();
     previousSlide();
 });
 next.addEventListener("click", () => {
-    stopInterval();
-    startInterval();
     nextSlide();
 });
-rowSlider.addEventListener("mouseover",stopInterval);
-rowSlider.addEventListener("mouseout", startInterval);
-rowSlider.addEventListener("touchstart", (event) => {touchStart(event)});
-rowSlider.addEventListener("touchend", (event) => {touchEnd(event)});
-
-startInterval();
