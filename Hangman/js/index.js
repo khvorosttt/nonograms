fefd367da4e55001caacddd259f696c760429ff9
khvorosttt <khvorosttt@yandex.by>
2023-12-27 02:12:48 +0300

    feat: add file with questions

---
+++ Hangman/js/index.js
@@ -0,0 +1,4 @@
+import info from '../data/questionsAnswers.json' assert { type: "json" };
+import gameHTML from './initGameHTML.js';
+
+document.body.insertAdjacentHTML('beforeend', gameHTML);
---
+++ Hangman/js/initGameHTML.js
@@ -0,0 +1,20 @@
+const gameHTML = () => `
+    <div class="game">
+        <div class="wrapper">
+            <div class="hangman">
+            </div>
+            <div class="hangman__content">
+                <div class="word">
+                kjhjhg
+                </div>
+                <div class="hint">
+                </div>
+                <div class="incorrectGuesses">
+                </div>
+                <div class="virtualKeyboard">
+                </div>
+            </div>
+        </div>
+    </div>
+`
+export default gameHTML;
