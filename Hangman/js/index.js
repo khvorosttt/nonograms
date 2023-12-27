cea023dfb0d74b1bd6d01a330413ce0845827264
khvorosttt <khvorosttt@yandex.by>
2023-12-27 20:59:23 +0300

    refactor: add function initGame

--- Hangman/js/index.js
+++ Hangman/js/index.js
@@ -6,54 +6,68 @@ const letters = [
     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
     'V', 'W', 'X', 'Y', 'Z'
 ];
-const clickedLetters = [];
-let numberQuestion = randomNumber(info.length);
-let incorrectGuesses = 0;
-let question = info[numberQuestion].question;
-let answer = info[numberQuestion].answer.toUpperCase();
-document.body.insertAdjacentHTML('beforeend', gameHTML(question, incorrectGuesses));
-let word = document.querySelector('.word');
-const virtualKeyboard = document.querySelector('.virtualKeyboard');
-for (let i = 0; i < letters.length; i++) {
-    virtualKeyboard.insertAdjacentHTML('beforeend',
-        `<button id='letter' class="letter">${letters[i]}</button>`
-    );
+let clickedLetters;
+let numberQuestion;
+let incorrectGuesses;
+let question;
+let answer;
+let word;
+let virtualKeyboard;
+let guesses;
+let answerLetters;
+let keyboardLetters;
+
+function initGame() {
+    document.body.replaceChildren();
+    clickedLetters = [];
+    numberQuestion = randomNumber(info.length);
+    incorrectGuesses = 0;
+    question = info[numberQuestion].question;
+    answer = info[numberQuestion].answer.toUpperCase();
+    document.body.insertAdjacentHTML('beforeend', gameHTML(question, incorrectGuesses));
+    word = document.querySelector('.word');
+    virtualKeyboard = document.querySelector('.virtualKeyboard');
+    addVirtualKeyboard();
+    hideLetters();
+    guesses = document.querySelector('.falseGuesses');
+    answerLetters = document.querySelectorAll('.answerLetter');
+    keyboardLetters = document.querySelectorAll('.letter');
+    keyboardLetters.forEach( (item) => {
+        item.addEventListener('click', (event) => {
+            letterClick(event);
+        }, {once: true});
+    });
+    document.addEventListener('keydown', (event) => {
+        const key = event.key.toUpperCase();
+        if (!event.altKey && !event.shiftKey && !event.ctrlKey) {
+            if (letters.includes(key)) {
+                if(!clickedLetters.includes(key)) {
+                    ltrClick(key, keyboardLetters[letters.indexOf(key)]);
+                }
+            }
+        }
+    });
 }
 
 function randomNumber(limit) {
     return Math.floor(limit * Math.random());
 }
-console.log(word);
-console.log(answer);
-for (let i = 0; i < answer.length; i++) {
-    word.insertAdjacentHTML('beforeend',
-        `<span class="answerLetter">__</span>`
-    );
+
+function addVirtualKeyboard() {
+    for (let i = 0; i < letters.length; i++) {
+        virtualKeyboard.insertAdjacentHTML('beforeend',
+            `<button id='letter' class="letter">${letters[i]}</button>`
+        );
+    }
 }
 
-const guesses = document.querySelector('.falseGuesses');
-const answerLetters = document.querySelectorAll('.answerLetter');
-const keyboardLetters = document.querySelectorAll('.letter');
-keyboardLetters.forEach( (item) => {
-    item.addEventListener('click', (event) => {
-        letterClick(event);
-    }, {once: true});
-})
-
-document.addEventListener('keydown', (event) => {
-    const key = event.key.toUpperCase();
-    if (!event.altKey && !event.shiftKey && !event.ctrlKey) {
-        if (letters.includes(key)) {
-            if(!clickedLetters.includes(key)) {
-                ltrClick(key, keyboardLetters[letters.indexOf(key)]);
-            }
-        }
+function hideLetters() {
+    for (let i = 0; i < answer.length; i++) {
+        word.insertAdjacentHTML('beforeend',
+            `<span class="answerLetter">__</span>`
+        );
     }
-    console.log(clickedLetters);
-})
-
-
-
+}
 
 function letterClick(event) {
     const currentElem = event.currentTarget;
@@ -74,5 +88,11 @@ function ltrClick(clickLetter, currentElem) {
     }
     currentElem.classList.add('letterClicked');
     clickedLetters.push(clickLetter);
-    console.log(clickedLetters);
 }
+
+
+window.addEventListener('load', initGame);
+
+
+
+
